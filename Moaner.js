07a4e9ef8948acb9440993fca399e8a23ce388da
FREEDOM <vinxad@gmail.com>
2021-06-27 12:45:08 +0200

    no idea what im doing 4

--- Moaner.js
+++ Moaner.js
@@ -9,10 +9,11 @@ function init() {
     sensor.start();
 }
 
-
+let start = new Date().getTime();
 sensor.onreading = () => {
     upOrDown();
 }
+let started = false;
 
 function upOrDown(){
     let x = sensor.x - 9.82;
@@ -20,12 +21,25 @@ function upOrDown(){
 
     if (x < -margin){
         document.getElementById("x").innerHTML = "UP";
+        bpm();
     }
     else if (x > margin) {
         document.getElementById("x").innerHTML = "DOWN";
     }
     else {
         document.getElementById("x").innerHTML = "STILL";
+    }
+
+
+}
+function bpm(){
+    if (started){
+        started = false;
+        let elapsed = (new Date().getTime() - start)*1000;
+        document.getElementById("bpm").innerHTML = 60/elapsed;
+    }
+    else {
+        start = new Date().getTime();
     }
 
 }
