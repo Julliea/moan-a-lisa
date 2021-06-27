04f7307d32282a65fa76e8ade8f32081455f309e
FREEDOM <vinxad@gmail.com>
2021-06-27 12:18:59 +0200

    no idea what im doing 3

--- Moaner.js
+++ Moaner.js
@@ -12,8 +12,6 @@ function init() {
 
 sensor.onreading = () => {
     document.getElementById("x").innerHTML = parseFloat(sensor.x).toFixed(2);
-    document.getElementById("y").innerHTML = parseFloat(sensor.y).toFixed(2);
-    document.getElementById("z").innerHTML = parseFloat(sensor.z).toFixed(2);
 }
 
 sensor.onerror = event => console.log(event.error.name, event.error.message);
