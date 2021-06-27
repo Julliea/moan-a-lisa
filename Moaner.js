19ca67882c8fa6dede146280596dbcc1bdfc7e9e
FREEDOM <vinxad@gmail.com>
2021-06-27 11:37:59 +0200

    no idea what im doing

--- Moaner.js
+++ Moaner.js
@@ -13,6 +13,9 @@ sensor.onreading = () => {
     console.log("Acceleration along X-axis: " + sensor.x);
     console.log("Acceleration along Y-axis: " + sensor.y);
     console.log("Acceleration along Z-axis: " + sensor.z);
+    document.getElementById("x").innerHTML = sensor.x;
+    document.getElementById("y").innerHTML = sensor.y;
+    document.getElementById("z").innerHTML = sensor.z;
 }
 
 sensor.onerror = event => console.log(event.error.name, event.error.message);
