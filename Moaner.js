34e9719e7167b090f307a374b64016602adae042
FREEDOM <vinxad@gmail.com>
2021-07-12 22:51:24 +0200

    innerHTML -> value

--- Moaner.js
+++ Moaner.js
@@ -25,6 +25,7 @@ function upOrDown(){
     let x = sensor.x
     let margin = 2;
 
+    document.getElementById("rrate").innerHTML = slider.value;
     //document.getElementById("x").innerHTML = x.toString();
     if (x < -margin){
         document.getElementById("x").innerHTML = "UP";
